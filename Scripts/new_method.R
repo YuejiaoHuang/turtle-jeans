bfcb6cddd5d591e5909f9b941f40647ab9633d53
Jule Drewalowski <jule@Jules-MacBook-Pro.local>
2024-11-28 16:25:13 +0100

    new ideas new method

--- Scripts/new_method.R
+++ Scripts/new_method.R
@@ -1,3 +1,8 @@
+library(tidyverse)
+library(ape)
+library(phylter)
+
+
 #################
 ### LOAD DATA ###
 #################
@@ -21,7 +26,7 @@ locus.trees <- locus.trees[Ntip(locus.trees) >= 9]
 
 
 # RUNNN phylteR to get outliers
-results <- phylter(locus.trees_combined, gene.names = names_combined)
+results <- phylter(locus.trees, gene.names = names)
 
 
 # get matrix for each gene that contains pairwise distances between species
@@ -33,10 +38,21 @@ matrices <- results$Initial$mat.data
 # for taxon 1: compare row for taxon1 between all genes
 # ...
 
+# for taxon 1: take mean distance to other taxa for each gene
+# plot all means
+# take outliers from that distribution
+# -> Outlier genes for taxa 1
+
 ## OUTLIER GENES
 # for build distribution for each gene (based on values for all pairwise distances)
-#     plot all pariwise distances from upper diagonal of matrix
-#     fit parameters for that distribution
+#     plot all pariwise distances from upper triangle of matrix
+#     fit parameters for that distribution ISSUE: WHICH DISTRIBUTIONS, HOW TO COMPARE DISTRIBUTIONS
 # which distribution is different (based on comparing parameters)?
 
+# get upper triangle
+matrices_example <- matrices$`100088at32523`
+matrices_example_vector<-as.vector(matrices_example[upper.tri(matrices_example)])
 
+data_frame(val = matrices_example_vector) %>%
+  ggplot(., aes(val)) + 
+  geom_density()
