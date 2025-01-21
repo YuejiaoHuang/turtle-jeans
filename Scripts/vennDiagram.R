364147d053dff1124ffd21c67aa0a49c2bd82c0c
wessel-mulder <wesselmulder00@gmail.com>
2025-01-21 20:25:40 +0100

    made venn diagram

--- Scripts/vennDiagram.R
+++ Scripts/vennDiagram.R
@@ -1,67 +1,59 @@
-library(VennDiagram)
+library('ggvenn')
 
-# gene vs list 
-meangene <- read.csv('Results/outliergene_q95_meangene.csv',
+# genes
+gene_dnds <- read.csv('Results/outliers_genes_dnds_q95.csv',
                      col.names = 'gene')
-meanlist <- read.csv('Results/outliergene_q95_meanlist.csv',
-                     col.names='gene')
+gene_dn <- read.csv('Results/outliers_genes_dn_q95.csv',
+                      col.names = 'gene')
+gene_ds <- read.csv('Results/outliers_genes_ds_q95.csv',
+                      col.names = 'gene')
+gene_pure <- read.csv('Results/outliers_genes_pure_q95.csv',
+                      col.names = 'gene')
 
 # Prepare a palette of 3 colors with R colorbrewer:
-cols <- c('#2274a5','#E6AF2E')
+cols <- c('purple','red','blue','green')
 
 # Chart
-ggvenn(data = list('Gene' = meangene$gene,
-                   'List' = meanlist$gene),
+ggvenn(data = list('dNdS' = gene_dnds$gene,
+                   'dN' = gene_dn$gene,
+                   'dS' = gene_ds$gene,
+                   'Pure' = gene_pure$gene),
        fill_color = cols,
-       show_percentage = F)+
+       show_percentage = T)+
   scale_y_continuous(expand = expansion(mult = .3))
 
 
-ggsave('Results/venn_genelist_quant.pdf',last_plot(),
-       width = 4,
-       height = 4)
+ggsave('Results/venn_outliergenes.pdf',last_plot(),
+       width = 8,
+       height = 8)
 
-
-#### GENE
-# chi vs quan
-chi <- read.csv('Results/outliergene_chi95_meangene.csv',
-                     col.names = c('gene'))
-quan <- read.csv('Results/outliergene_q95_meangene.csv',
-                     col.names = c('gene'))
-
-# Prepare a palette of 3 colors with R colorbrewer:
-cols <- c('#A0C4D8','#083047')
-
-# Chart
-
-ggvenn(data = list('ChiSq' = chi$gene,
-                   'Quantile' = quan$gene),
-       fill_color = cols,
-       show_percentage = F)+
-  scale_y_continuous(expand = expansion(mult = .3))
-
-ggsave('Results/venn_gene_chiquant.pdf',last_plot())
-
-#### List
-# chi vs quan
-chi <- read.csv('Results/outliergene_chi95_meanlist.csv',
-                col.names = c('gene'))
-quan <- read.csv('Results/outliergene_q95_meanlist.csv',
-                 col.names = c('gene'))
+### REPEAT WITH SPECIES
+# genes
+species_dnds <- read.csv('Results/outliers_species_dnds_q95.csv',
+                      col.names = 'gene')
+species_dn <- read.csv('Results/outliers_species_dn_q95.csv',
+                    col.names = 'gene')
+species_ds <- read.csv('Results/outliers_species_ds_q95.csv',
+                    col.names = 'gene')
+species_pure <- read.csv('Results/outliers_species_pure_q95.csv',
+                      col.names = 'gene')
 
 # Prepare a palette of 3 colors with R colorbrewer:
-cols <- c('#DCCAA1','#6E4F08')
+cols <- c('purple','red','blue','green')
 
 # Chart
-
-ggvenn(data = list('ChiSq' = chi$gene,
-                   'Quantile' = quan$gene),
+ggvenn(data = list('dNdS' = species_dnds$gene,
+                   'dN' = species_dn$gene,
+                   'dS' = species_ds$gene,
+                   'Pure' = species_pure$gene),
        fill_color = cols,
-       show_percentage = F)+
+       show_percentage = T)+
   scale_y_continuous(expand = expansion(mult = .3))
 
-ggsave('Results/venn_list_chiquant.pdf',last_plot())
 
+ggsave('Results/venn_outlierspecies.pdf',last_plot(),
+       width = 8,
+       height = 8)
 
 
 
